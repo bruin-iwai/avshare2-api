1b61aac19ba6d98baa601d0ff58b1dce4b2a3de0
Katsuhito Iwai <bruin.iwai@gmail.com>
2021-07-04 16:03:43 +0900

    AWS SDK v3 の s3 GetObject がデータを直接返さなくなった件の対応

--- app/listUrls.js
+++ app/listUrls.js
@@ -1,5 +1,6 @@
 const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 const map = require('async/map');
+const getStream = require('get-stream');
 const generateSignedUrl = require('./generateSignedUrl');
 
 module.exports = async function listUrls(bucket, prefix) {
@@ -9,7 +10,9 @@ module.exports = async function listUrls(bucket, prefix) {
     Bucket: bucket,
     Key: `${prefix}/index.json`,
   });
-  const index = await client.send(command).then((ret) => JSON.parse(ret.Body.toString()));
+  const ret = await client.send(command);
+  const data = await getStream(ret.Body);
+  const index = JSON.parse(data);
 
   // index.jsonのfiles[].{file, title}を取得し、fileをsingedUrlに変換する
   const urls = await map(index.files, async (item) => {
