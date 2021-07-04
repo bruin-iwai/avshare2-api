3083ab6407d6bc695a74a63f5a9eb7b44d8b0e3b
Katsuhito Iwai <bruin.iwai@gmail.com>
2021-07-04 22:08:49 +0900

    XRay SDK 導入

--- app/listUrls.js
+++ app/listUrls.js
@@ -1,11 +1,12 @@
-const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { S3, GetObjectCommand } = require('@aws-sdk/client-s3');
+const AWSXRay = require('aws-xray-sdk');
 const map = require('async/map');
 const getStream = require('get-stream');
 const generateSignedUrl = require('./generateSignedUrl');
 
 module.exports = async function listUrls(bucket, prefix) {
   // S3からindex.jsonをダウンロード
-  const client = new S3Client();
+  const client = AWSXRay.captureAWSv3Client(new S3({}));
   const command = new GetObjectCommand({
     Bucket: bucket,
     Key: `${prefix}/index.json`,
